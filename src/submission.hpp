6faeee278265fb9812817b63d7a7804e400f39c2
poytaytoy <clementcycc8@gmail.com>
2026-09-08 18:01:43 -0400

    fix typo

--- src/submission.hpp
+++ src/submission.hpp
@@ -66,7 +66,7 @@ void apply_stencil(const Grid& old_grid, Grid& new_grid) {
 
   // loop to populate each entry with the heat spread formula  
 
-  #pragma omp parallel
+  #pragma omp parallel for schedule(static)
   for (std::size_t i = 1; i < rows - 1; ++i) {
     const double* top = old_data + (i - 1) * cols;
     const double* mid = old_data + i * cols;
