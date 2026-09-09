484e6ab7cdcba9f31512375fb95d632e37beb169
poytaytoy <clementcycc8@gmail.com>
2026-09-09 00:35:42 -0400

    bring in regular version if the distribution is irregular

--- src/submission.hpp
+++ src/submission.hpp
@@ -109,9 +109,7 @@ public:
 // Apply the five-point stencil over all interior points, copying the boundary
 // values unchanged from old_grid to new_grid. Implement your solution here.
 
-inline void apply_stencil(const Grid& old_grid, Grid& new_grid) {
-
-  old_grid.initialize_quantization_grid(); 
+inline void apply_stencil_quantized(const Grid& old_grid, Grid& new_grid) {
   new_grid.inherit_quantization(old_grid); 
 
   const std::size_t rows = old_grid.rows();
@@ -157,5 +155,58 @@ inline void apply_stencil(const Grid& old_grid, Grid& new_grid) {
       
       out_quantized[j] = quantized_result;  
     }
+  }
+}
+
+inline void apply_stencil_regular(const Grid& old_grid, Grid& new_grid) {
+
+  const std::size_t rows = old_grid.rows();
+  const std::size_t cols = old_grid.cols();
+
+  // generate the pointers 
+  const double* __restrict old_data = old_grid.data();
+  double* __restrict new_data = new_grid.data();
+
+
+  // copy the first and last row of the boundary from old to the new 
+  std::memcpy(
+    new_data,
+    old_data,
+    cols * sizeof(double)
+  );
+
+  std::memcpy(
+      new_data + (rows - 1) * cols,
+      old_data + (rows - 1) * cols,
+      cols * sizeof(double)
+  );
+
+  // loop to populate each entry with the heat spread formula  
+  #pragma omp parallel for schedule(static)
+  for (std::size_t i = 1; i < rows - 1; ++i) {
+    const double* top = old_data + (i - 1) * cols;
+    const double* mid = old_data + i * cols;
+    const double* bottom = old_data + (i + 1) * cols;
+    double* out = new_data + i * cols;
+    
+    // populate column boundary from old to new 
+    out[0] = mid[0];
+    out[cols - 1] = mid[cols - 1];
+
+    #pragma omp simd
+    for (std::size_t j = 1; j < cols - 1; ++j) {
+      out[j] = 0.5 * mid[j] + 0.125 * (top[j] + bottom[j] + mid[j - 1] + mid[j + 1]);
+    }
+  }
+}
+
+inline void apply_stencil(const Grid& old_grid, Grid& new_grid){
+  old_grid.initialize_quantization_grid(); 
+  new_grid.is_quantized_yet_ = false;
+
+  if (old_grid.bad_){
+    apply_stencil_regular(old_grid, new_grid); 
+  } else{ 
+    apply_stencil_quantized(old_grid, new_grid);
   }
 }
